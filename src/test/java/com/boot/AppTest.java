a7589208ebe427a0af62fdee05e9b7396f9b2e40
Robin Hopkins <robinjohnhopkins@hotmail.com>
2019-01-25 18:17:07 +0000

    added sprinf test, remove junit from pom. added a real junit test

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,20 +1,22 @@
 package com.boot;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.boot.controller.HomeController;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testHomeController()
     {
-        assertTrue( true );
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals("check home controller", result, "Das Boot, reporting for duty!" );
     }
 }
